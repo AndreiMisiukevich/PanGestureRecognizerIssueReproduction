2a75e8bf056b8f4838bfee879c9aefd01efaaeff
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-06-04 00:21:46 +0300

    updated sample to latest pre-release version

---
+++ PanIssueReproduction/PanIssueReproduction/App.cs
@@ -0,0 +1,95 @@
+﻿using Xamarin.Forms;
+using System.Linq;
+
+namespace PanIssueReproduction
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var first = new Button { Text = "Scroller" };
+            first.Clicked += (sender, e) => Navigation.PushAsync(new FirstPage());
+
+            var second = new Button { Text = "TapGesture" };
+            second.Clicked += (sender, e) => Navigation.PushAsync(new SecondPage());
+
+            Content = new StackLayout
+            {
+                Children = {
+                    first,
+                    second
+                }
+            };
+        }
+    }
+
+    public class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            var longItem = new Label
+            {
+                Text = new string(Enumerable.Range(0, 255).Select(x => (char)x).ToArray()),
+                FontSize = 60
+            };
+
+            var scroller = new ScrollView
+            {
+                Content = longItem
+            };
+
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += (sender, e) => {
+                DisplayAlert("I'm working!", "I'm working!", "I'm working!"); //Not calling :(
+            };
+
+            Content = new ContentView
+            {
+                Content = scroller,
+                GestureRecognizers = {
+                    panGesture
+                }
+            };
+        }
+    }
+
+    public class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            var tapGesture = new TapGestureRecognizer();
+            tapGesture.Tapped += (sender, e) => {
+                DisplayAlert("TAP", "TAP", "TAP");
+            };
+
+            var tapItem = new ContentView
+            {
+                BackgroundColor = Color.Gold,
+                GestureRecognizers = {
+                    tapGesture
+                }
+            };
+
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += (sender, e) => {
+                DisplayAlert("I'm working!", "I'm working!", "I'm working!"); //Not calling :(
+            };
+
+            Content = new ContentView
+            {
+                Content = tapItem,
+                GestureRecognizers = {
+                    panGesture
+                }
+            };
+        }
+    }
+}
